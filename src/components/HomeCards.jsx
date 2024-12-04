b8fb01f27c6a456af6f73faa5adb93f04469d954
ahmadhamdi2010 <ahmad2013hamdi@gmail.com>
2024-12-04 16:10:15 +0100

    added server functionality using JSON server, services now are being fetched via an API request and listed using the services compoenent

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import Card from './Card'
-import services from '../services.json'
+import { useState,useEffect } from 'react';
+
 
 const HomeCards = ({isHome =false}) => {
-  const keyService = isHome? services.slice(0,3): services;
+
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchServices = async ()=>{
+
+      try {
+        const res = await fetch('http://localhost:8000/services');
+        const data = await res.json();
+        setServices(data);
+        
+      } catch (error) {
+        console.log("error getting data", error);
+      }finally{
+        setLoading(false);
+      }
+      
+    }
+    fetchServices();
+  },[])
+
   return (
     <div className="bg-gray-50 py-24 sm:py-32">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-1">
-            {keyService.map((service)=>(
+            {loading? (<h2>Loading</h2>):(
+              <>
+              {services.map((service)=>(
                 <Card key={service.id} title={service.title} bodyText={service.description} destination={service.id} />
             ))}
+              </>
+            ) }
+            
         </div>
       </div>
     </div>
