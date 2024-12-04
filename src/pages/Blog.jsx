eff5b37185279c23977f8030526a26bf5a491033
ahmadhamdi2010 <ahmad2013hamdi@gmail.com>
2024-12-04 20:56:58 +0100

    Final Commit

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import BlogPostsLoader from '../components/BlogPostsLoader'
+import PageTitle from '../components/Pagetitle'
+
 
 const Blog = () => {
   return (
     <div>
+      <PageTitle title="Blog" />
       <BlogPostsLoader isHome={false}/>
     </div>
   )
